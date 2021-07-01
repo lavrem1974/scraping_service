ba2b78813910c7fe882436db95c0b88e9d6cc863
lavrem1974 <lavrem1974@gmail.com>
2021-07-01 20:39:57 +0300

    Initial content

---
+++ src/scraping_service/views.py
@@ -0,0 +1,10 @@
+
+from django.shortcuts import render
+import datetime
+
+
+def home(request):
+    date = datetime.datetime.now().date()
+    name = 'Michael'
+    datas = {'date':date, 'name':name}
+    return render(request,'home.html', datas)
